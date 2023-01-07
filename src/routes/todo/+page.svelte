853499654877455b31a20c99fbbe6f1f6cdd1399
pranay.bajracharya <law.pran99@gmail.com>
2023-01-07 13:52:03 +0545

    added default layout of sveltekit

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,66 +1,74 @@
 <script lang="ts">
-  import type {TodoSchema} from "src/schema/interface";
-  import TodoListItem from "./TodoListItem.svelte";
-  import '../styles.css';
-  
-  let text = "";
-  let error = ""
-  let todoList: TodoSchema[] = [];
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+	import autoAnimate from '@formkit/auto-animate';
+	import type { TodoSchema } from 'src/schema/interface';
+	import TodoListItem from './TodoListItem.svelte';
+	import '../styles.css';
 
-    if (text.trim() === "") {
-      error = "Required!";
-      return;
-    }
+	let text = '';
+	let error = '';
+	let todoList: TodoSchema[] = [];
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
 
-    todoList = [
-      ...todoList,
-      {
-        id: Math.random(),
-        name: text,
-      },
-    ];
-    text = "";
-  };
+		if (text.trim() === '') {
+			error = 'Required!';
+			return;
+		}
 
-  const removeError = () => {
-    error = "";
-  };
+		todoList = [
+			...todoList,
+			{
+				id: Math.random(),
+				name: text
+			}
+		];
+		text = '';
+	};
+
+	const autoAnimateTS = autoAnimate as any;
+
+	const removeError = () => {
+		error = '';
+	};
 </script>
 
 <main>
-  <h1>To do list</h1>
-  <div>
-    <form on:submit={handleSubmit}>
-      <input
-        type="text"
-        class={error ? "error" : ""}
-        bind:value={text}
-        on:input={removeError}
-      />
-    </form>
-  </div>
-  <ul>
-    {#each todoList as todo (todo.id)}
-      <TodoListItem {todo} />
-    {/each}
-  </ul>
+	<h1>To do list</h1>
+	<div class="container">
+		<form on:submit={handleSubmit}>
+			<input type="text" class={error ? 'error' : ''} bind:value={text} on:input={removeError} />
+		</form>
+	</div>
+	<ul use:autoAnimateTS>
+		{#each todoList as todo (todo.id)}
+			<TodoListItem {todo} />
+		{/each}
+	</ul>
 </main>
 
 <style>
-  h1 {
-    margin-block: 1rem;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-  input {
-    padding: 5px 5px;
-    outline: none;
-  }
-  input.error {
-    border: 2px solid red;
-  }
+	h1 {
+		margin-block: 1rem;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0;
+	}
+
+	input {
+		padding: 5px 5px;
+		outline: none;
+	}
+
+	input.error {
+		border: 2px solid red;
+	}
+
+	.container {
+		display: flex;
+		justify-content: center;
+		max-width: 1200px;
+		margin: auto;
+	}
 </style>
