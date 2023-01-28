bc94ef4d272324857c6bf48c0667da1d63c2e584
pranay.bajracharya <law.pran99@gmail.com>
2023-01-28 18:24:35 +0545

    tech svgs and flip animation

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	import logo from '$lib/icons/svelte.svg';
+	import github from '$lib/icons/github.svg';
 </script>
 
 <header>
--- src/routes/memory/Tile.svelte
+++ src/routes/memory/Tile.svelte
@@ -1,7 +1,46 @@
 <script lang="ts">
-	import logo from '$lib/images/svelte-logo.svg';
+	import svelte from '$lib/icons/svelte.svg';
+	import astro from '$lib/icons/astro.svg';
+
+	let frontFaced = true;
+
+	const handleFlipTile = () => {
+		frontFaced = !frontFaced;
+	};
 </script>
 
-<div class="grid place-items-center aspect-square max-h-[10vh] rounded-full bg-slate-100">
-  <img class="w-[50%]" src={logo} alt="Svelte" />
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="aspect-square max-h-[10vh] rounded-full relative select-none" on:click={handleFlipTile}>
+	<div
+		class="grid place-items-center h-full w-full rounded-full bg-slate-100 tile"
+		class:front-face={frontFaced}
+		class:back-face={!frontFaced}
+	>
+		<img class="w-[50%]" src={svelte} alt="Svelte" />
+	</div>
+	<div
+		class="grid place-items-center h-full w-full rounded-full bg-slate-600 tile"
+		class:front-face={!frontFaced}
+		class:back-face={frontFaced}
+	>
+		<img class="w-[50%]" src={astro} alt="Svelte" />
+	</div>
 </div>
+
+<style>
+	.tile {
+		backface-visibility: hidden;
+		transform-style: preserve-3d;
+		transition: all 0.5s ease-in-out;
+		top: 0;
+		left: 0;
+	}
+	.front-face {
+		position: relative;
+		transform: rotateY(0deg);
+	}
+	.back-face {
+		position: absolute;
+		transform: rotateY(180deg);
+	}
+</style>
