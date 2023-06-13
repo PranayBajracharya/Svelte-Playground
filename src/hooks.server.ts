3574df58325ff67342ae12e3c7f0a9077b9a3e2a
pranay.bajracharya <law.pran99@gmail.com>
2023-06-13 20:54:05 +0545

    update packages

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,15 @@
-import { SvelteKitAuth } from '@auth/sveltekit';
+import { GITHUB_ID, GITHUB_SECRET, SERVICE_ROLE_SECRET, SUPABASE_URL } from '$env/static/private';
 import GitHub from '@auth/core/providers/github';
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import { SvelteKitAuth } from '@auth/sveltekit';
+import { SupabaseAdapter } from '@next-auth/supabase-adapter';
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
-// import { SupabaseAdapter } from '@next-auth/supabase-adapter';
 
 const handleAuth = SvelteKitAuth({
-	// adapter: SupabaseAdapter({
-	// 	url: SUPABASE_URL,
-	// 	secret: SERVICE_ROLE_SECRET
-	// }) as any,
+	adapter: SupabaseAdapter({
+		url: SUPABASE_URL,
+		secret: SERVICE_ROLE_SECRET
+	}) as any,
 	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }) as any]
 }) satisfies Handle;
 
--- src/lib/db/supabaseClient.ts
+++ src/lib/db/supabaseClient.ts
@@ -1,4 +1,4 @@
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '$env/static/private';
 import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
